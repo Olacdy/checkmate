ae8046f1623020d6b9e9224f2082a628c6db1eb1
Олег Дидечкин <crm.29431@dz.com>
2023-08-31 13:04:19 +0300

    Added colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,10 @@
 import { FC } from 'react';
 
-import { serverClient } from '@/trpc/server';
-
-import SignInButton from '@/components/SignInButton';
-import TodoList from '@/components/TodoList';
-
 type pageProps = {};
 
 const page: FC<pageProps> = async ({}) => {
-  const todos = await serverClient.todo.getTodos();
-
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <SignInButton />
-      <TodoList initialTodos={todos} />
-    </main>
+    <main className='flex min-h-screen flex-col items-center justify-between max-w-7xl mx-auto'></main>
   );
 };
 
