21e7362eaebd53af3e72fe42667e6ac37bc0f357
Олег Дидечкин <crm.29431@dz.com>
2023-09-07 18:34:18 +0300

    Refactoring

--- src/components/auth/oauth-buttons.tsx
+++ src/components/auth/oauth-buttons.tsx
@@ -9,12 +9,8 @@ import { Button } from '@/components/ui/button';
 
 import { cn } from '@/lib/utils';
 
-const oauthProviders = [
-  { name: 'google', icon: 'google' },
-  { name: 'github', icon: 'githubAuth' },
-] satisfies {
-  name: string;
-  icon: keyof typeof Icons;
+const oauthProviders = [{ name: 'google' }, { name: 'github' }] satisfies {
+  name: keyof typeof Icons;
 }[];
 
 type OAuthButtonsProps = {};
@@ -39,7 +35,7 @@ const OAuthButtons: FC<OAuthButtonsProps> = ({}) => {
   return (
     <div className='grid w-full grid-cols-1 gap-3'>
       {oauthProviders.map((provider) => {
-        const Icon = Icons[provider.icon];
+        const Icon = Icons[provider.name];
 
         return (
           <Button
@@ -50,7 +46,7 @@ const OAuthButtons: FC<OAuthButtonsProps> = ({}) => {
             <div className='relative flex items-center justify-center gap-2'>
               <Icon
                 className={cn('h-5 w-5 fill-off-white dark:fill-oxford-blue', {
-                  '-mr-1 mb-1 h-6 w-6': provider.name === 'github',
+                  '-mr-1 mb-1 h-7 w-7': provider.name === 'github',
                 })}
               />
               <span className='text-lg capitalize dark:font-semibold'>
