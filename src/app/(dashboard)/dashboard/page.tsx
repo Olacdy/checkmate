33ce399399bcf4d41b99a62f961f41503a1e2899
Олег Дидечкин <crm.29431@dz.com>
2023-09-08 18:44:24 +0300

    Dashboard content setup

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -5,7 +5,11 @@ import { FC } from 'react';
 type pageProps = {};
 
 const page: FC<pageProps> = ({}) => {
-  return <div>dashboard</div>;
+  return (
+    <section className='flex flex-1 p-4 pl-0'>
+      <div className='flex-1 rounded-lg bg-slate-300 dark:bg-oxford-blue-dark'></div>
+    </section>
+  );
 };
 
 export default page;
