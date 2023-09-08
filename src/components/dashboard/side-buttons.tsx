acf5f3e7a17f46266ca605b23de76a549c089c6f
Олег Дидечкин <crm.29431@dz.com>
2023-09-08 18:39:34 +0300

    Semantics corrected

--- src/components/dashboard/side-buttons.tsx
+++ src/components/dashboard/side-buttons.tsx
@@ -19,52 +19,54 @@ const SideButtons: FC<SideButtonsProps> = ({}) => {
   const pathname = usePathname();
 
   return (
-    <div className='flex w-full flex-1 flex-col items-center gap-3'>
+    <ul className='flex w-full flex-1 flex-col items-center gap-3'>
       {sideBarButtons.map((button) => {
         const Icon = Icons[button.icon];
 
         return (
-          <Link
-            className={cn('w-full', {
-              'pointer-events-none': pathname === button.path,
-            })}
-            href={button.path}>
-            <Button
-              disabled={pathname === button.path}
-              variant={pathname === button.path ? 'default' : 'ghost'}
-              className={cn(
-                'flex w-full items-center justify-start gap-4 py-7 disabled:opacity-100',
-                {
-                  'bg-oxford-blue/90 dark:bg-off-white':
-                    pathname === button.path,
-                  'hover:bg-slate-300 dark:hover:bg-slate-200/30':
-                    pathname !== button.path,
-                }
-              )}>
-              <Icon
+          <li className='w-full' key={button.name}>
+            <Link
+              className={cn('w-full', {
+                'pointer-events-none': pathname === button.path,
+              })}
+              href={button.path}>
+              <Button
+                disabled={pathname === button.path}
+                variant={pathname === button.path ? 'default' : 'ghost'}
                 className={cn(
-                  'h-7 w-7 fill-transparent stroke-oxford-blue/70 dark:stroke-off-white',
+                  'flex w-full items-center justify-start gap-4 py-7 disabled:opacity-100',
                   {
-                    'stroke-off-white dark:stroke-oxford-blue/70':
+                    'bg-oxford-blue/90 dark:bg-off-white':
                       pathname === button.path,
-                  }
-                )}
-              />
-              <span
-                className={cn(
-                  'text-lg text-oxford-blue/70 dark:text-off-white',
-                  {
-                    'text-off-white dark:font-semibold dark:text-oxford-blue/90':
-                      pathname === button.path,
+                    'hover:bg-slate-300 dark:hover:bg-slate-200/30':
+                      pathname !== button.path,
                   }
                 )}>
-                {button.text}
-              </span>
-            </Button>
-          </Link>
+                <Icon
+                  className={cn(
+                    'h-7 w-7 fill-transparent stroke-oxford-blue/70 dark:stroke-off-white',
+                    {
+                      'stroke-off-white dark:stroke-oxford-blue/70':
+                        pathname === button.path,
+                    }
+                  )}
+                />
+                <span
+                  className={cn(
+                    'text-lg text-oxford-blue/70 dark:text-off-white',
+                    {
+                      'text-off-white dark:font-semibold dark:text-oxford-blue/90':
+                        pathname === button.path,
+                    }
+                  )}>
+                  {button.text}
+                </span>
+              </Button>
+            </Link>
+          </li>
         );
       })}
-    </div>
+    </ul>
   );
 };
 
