28156c706941882cd8e11d29c24a1db22e0f0138
Олег Дидечкин <crm.29431@dz.com>
2023-10-07 15:53:59 +0300

    Added proper light mode for validation data page

--- src/app/(validations)/validation/[id]/page.tsx
+++ src/app/(validations)/validation/[id]/page.tsx
@@ -18,7 +18,7 @@ const Page: FC<PageProps> = async ({ params }) => {
   return (
     <main className='flex h-screen max-h-screen items-center justify-center overflow-hidden'>
       <div className='flex max-h-[70%] w-full max-w-sm px-5 text-xs lg:max-w-xl lg:text-sm xl:max-w-2xl xl:text-base'>
-        <pre className='w-full flex-1 overflow-auto rounded-md border-2 border-slate-400 bg-oxford-blue p-8 dark:border-slate-700 dark:bg-oxford-blue-dark dark:text-off-white'>
+        <pre className='w-full flex-1 overflow-auto rounded-md border-2 border-slate-400 bg-slate-300 p-8 text-oxford-blue-dark dark:border-slate-700 dark:bg-oxford-blue-dark'>
           {JSON.stringify(validation.input, null, 2)}
         </pre>
       </div>
